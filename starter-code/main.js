77b51a4beff7f4cb69dfd96c4422c07155ce8d5e
Musicalaudio <jordandsouza352@gmail.com>
2023-08-07 16:57:07 +1200

    added copy mechanism to button

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,9 @@
+let copy = document.querySelector(".copy-btn");
 let rangeInput = document.querySelector("#char-length");
 let charLength = document.querySelector(".char-length__number");
 let form = document.querySelector("form");
-let pwStrength = 0;
+let meter = document.querySelector(".pw__strength-meter");
+let pwStrengthScore = 0;
 
 window.onload = function () {
   charLength.innerText = rangeInput.value;
@@ -14,7 +16,44 @@ rangeInput.addEventListener("input", (e) => {
 form.addEventListener("click", (e) => {
   let target = e.target;
   if (target.name === "pw__criteria") {
-    target.checked === true ? pwStrength++ : pwStrength--;
+    target.checked === true ? pwStrengthScore++ : pwStrengthScore--;
   }
-  console.log(pwStrength);
+  passwordStrength(pwStrengthScore);
 });
+
+copy.addEventListener("click", (e) => {
+  let copyText = document.querySelector(".generated-pw").innerText;
+  let inputElement = document.createElement("input");
+  inputElement.setAttribute("value", copyText);
+  document.body.appendChild(inputElement);
+  inputElement.select();
+  document.execCommand("copy");
+  inputElement.parentNode.removeChild(inputElement);
+  document.querySelector(".pw__is-copied").innerText = "COPIED";
+});
+
+function passwordStrength(score) {
+  let result = "";
+  let meterClr = "";
+  switch (score) {
+    case 1:
+      result = "TOO WEAK!";
+      meterClr = "too-weak";
+      break;
+    case 2:
+      result = "WEAK!";
+      meterClr = "weak";
+      break;
+    case 3:
+      result = "MEDIUM";
+      meterClr = "medium";
+      break;
+    case 4:
+      result = "STRONG";
+      meterClr = "strong";
+      break;
+  }
+  document.querySelector(".pw__strength-lvl").innerText = result;
+  meter.classList.remove("too-weak", "weak", "medium", "strong");
+  if (meterClr !== "") meter.classList.add(meterClr);
+}
